25bc719f8fb47ac32d8fbe074742f3a81b2dd26e
nmt11062005 <85308651+nmt11062005@users.noreply.github.com>
2021-06-04 00:54:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ const CONFIG = {
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Gửi cho anh <3😿',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều😽😽',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //https://www.facebook.com/profile.php?id=100026429376764
+    messLink: 'https://m.me/M.thuan1106'
 }
